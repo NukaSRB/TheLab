b18d12cd6297b940d7418c83b54a2d91cf82d86a
stygiansabyss <stygian.warlock.v2@gmail.com>
2016-12-29 09:50:25 -0600

    Adding migration

---
+++ database/migrations/2016_12_29_145338_create_scheduled_hours_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateScheduledHoursTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('scheduled_hours', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned()->index();
+            $table->integer('client_id')->unsigned()->index();
+            $table->date('date')->index();
+            $table->smallInteger('hours')->unsigned();
+            $table->text('note')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('user_id')
+                  ->references('id')
+                  ->on('users')
+                  ->onDelete('cascade');
+
+            $table->foreign('client_id')
+                  ->references('id')
+                  ->on('clients')
+                  ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('scheduled_hours');
+    }
+}
