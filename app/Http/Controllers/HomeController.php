990af7afee41c7ad9f8a6c37bc0a10c0b363c815
stygiansabyss <stygian.warlock.v2@gmail.com>
2017-01-02 13:33:36 -0600

    Removing test code

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,20 +11,6 @@ class HomeController extends BaseController
     {
         $this->setPageTitle('JumpGate Demo');
 
-        // $user = $asana->findTeamsByOrganization(8744492949110);
-        // dump(count($user));
-        // foreach ($user as $team) {
-        //     dump($team);
-        // }
-
-        // $clients = collect($toggle->handle('GetClients', ['page' => 2]));
-        // dump($clients->pluck('name', 'id'));
-
-        $time = $toggle
-            ->setApiKey(env('A_TOGGL_KEY'))
-            ->handle('GetCurrentTimeEntry');
-        dump($time);
-
         return $this->view();
     }
 }
