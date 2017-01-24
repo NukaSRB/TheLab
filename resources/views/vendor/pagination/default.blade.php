abcb0aa8eceb528edfd2a85c31890fe405ea96e9
stygiansabyss <stygian.warlock.v2@gmail.com>
2017-01-24 09:26:09 -0600

    Fixing pagination for bulimia updates

--- resources/views/vendor/pagination/default.blade.php
+++ resources/views/vendor/pagination/default.blade.php
@@ -1,36 +1,40 @@
 @if ($paginator->hasPages())
-    <nav class="pagination">
-        {{-- Previous Page Link --}}
-        @if ($paginator->onFirstPage())
-            <a class="button disabled"><span>&laquo;</span></a>
-        @else
-            <a class="button" href="{{ $paginator->previousPageUrl() }}" rel="prev">&laquo;</a>
-        @endif
+  <nav class="pagination is-centered">
+    {{-- Previous Page Link --}}
+    @if ($paginator->onFirstPage())
+      <a class="pagination-previous disabled"><span>&laquo;</span></a>
+    @else
+      <a class="pagination-previous" href="{{ $paginator->previousPageUrl() }}" rel="prev">&laquo;</a>
+    @endif
 
-        {{-- Pagination Elements --}}
-        @foreach ($elements as $element)
-            {{-- "Three Dots" Separator --}}
-            @if (is_string($element))
-                <a class="button disabled"><span>{{ $element }}</span></a>
-            @endif
+    <ul class="pagination-list">
+      {{-- Pagination Elements --}}
+      @foreach ($elements as $element)
+        <li>
+          {{-- "Three Dots" Separator --}}
+          @if (is_string($element))
+            <a class="pagination-ellipsis disabled"><span>{{ $element }}</span></a>
+          @endif
 
-            {{-- Array Of Links --}}
-            @if (is_array($element))
-                @foreach ($element as $page => $url)
-                    @if ($page == $paginator->currentPage())
-                        <a class="button is-primary"><span class="text-white">{{ $page }}</span></a>
-                    @else
-                        <a class="button" href="{{ $url }}">{{ $page }}</a>
-                    @endif
-                @endforeach
-            @endif
-        @endforeach
+          {{-- Array Of Links --}}
+          @if (is_array($element))
+            @foreach ($element as $page => $url)
+              @if ($page == $paginator->currentPage())
+                <a class="pagination-link is-current"><span class="text-white">{{ $page }}</span></a>
+              @else
+                <a class="pagination-link" href="{{ $url }}">{{ $page }}</a>
+              @endif
+            @endforeach
+          @endif
+        </li>
+      @endforeach
+    </ul>
 
-        {{-- Next Page Link --}}
-        @if ($paginator->hasMorePages())
-            <a class="button" href="{{ $paginator->nextPageUrl() }}" rel="next">&raquo;</a>
-        @else
-            <a class="button disabled"><span>&raquo;</span></a>
-        @endif
-    </nav>
+    {{-- Next Page Link --}}
+    @if ($paginator->hasMorePages())
+      <a class="pagination-next" href="{{ $paginator->nextPageUrl() }}" rel="next">&raquo;</a>
+    @else
+      <a class="pagination-next disabled"><span>&raquo;</span></a>
+    @endif
+  </nav>
 @endif
